297d7b39620e85f09bf06779742ccbca8db2f886
Bodolica Razvan <97350227+BodolicaRazvan@users.noreply.github.com>
2023-05-31 19:05:31 +0300

    Update API

--- api/endpoints.js
+++ api/endpoints.js
@@ -1,4 +1,4 @@
-const myAPI = 'c50768732c26a7606c57438beb12b595';
+const myAPI = '11336f700480d6a9e8aa99804ade5a71';
 
 function getCurrentWeatherEndPoint(city){
     return 'https://api.openweathermap.org/data/2.5/weather?q='+city+'&lang=ro&units=metric&appid='+ myAPI;
